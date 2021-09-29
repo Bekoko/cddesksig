9aa8e36d04ce5f5bb735e5a111b2af8ae392d912
Bernard Klacar <bernard.klacar@etu.unige.ch>
2021-09-29 12:35:32 +0200

    add .bash 2

---
+++ README.md
@@ -0,0 +1,11 @@
+//sumcheck of code
+
+$ shasum cddesksig/
+
+da39a3ee5e6b4b0d3255bfef95601890afd80709  cddesksig/
+
+
+
+// installation
+
+bash install.bash
--- install.bash
+++ install.bash
@@ -2,12 +2,12 @@
 sudo apt-get update
 
 # base lib
-sudo apt-get install python3.6
-sudo apt install python3-pip
-sudo apt-get install git
+sudo apt-get install python3.6 -y
+sudo apt install python3-pip -y
+sudo apt-get install git -y
 sudo curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt install nodejs
-sudo apt-get install network-manager
+sudo apt install nodejs -y
+sudo apt-get install network-manager -y
 systemctl start NetworkManager.service 
 systemctl enable NetworkManager.service
 
@@ -19,7 +19,7 @@ source env_cddesksig/bin/activate
 git clone https://github.com/Bekoko/cddesksig.git
 
 # dependencies
-pip install -r cddesksig/requirements.txt
+pip install -r cddesksig/requirements.txt -y
 
 # ganache-cli
 git clone https://github.com/trufflesuite/ganache.git
