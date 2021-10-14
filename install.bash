2392d649f83d59113699574036599c4e793db08e
Bernard Klacar <bernard.klacar@etu.unige.ch>
2021-10-14 12:05:08 +0200

    update install.bash

--- install.bash
+++ install.bash
@@ -1,12 +1,12 @@
 #bash
 sudo apt-get update
 
+# app
+git clone https://github.com/Bekoko/cddesksig.git
+
 # venv
 python3 -m venv env_cddesksig
 source env_cddesksig/bin/activate
-
-# app
-git clone https://github.com/Bekoko/cddesksig.git
 
 # dependencies
 pip install -r cddesksig/requirements.txt -y
