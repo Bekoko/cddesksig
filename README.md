62ee7ad39d3e3ee761b5281f838febe5425f37a9
Bernard Klacar <bernard.klacar@etu.unige.ch>
2021-10-15 20:35:15 +0200

    beta ready

--- README.md
+++ README.md
@@ -8,4 +8,4 @@ da39a3ee5e6b4b0d3255bfef95601890afd80709  cddesksig/
 
 // installation
 
-bash install.bash
+bash cddesksig_install.bash
--- cddesksig_install.sh
+++ cddesksig_install.sh
@@ -1,16 +1,32 @@
 #bash
+
+sudo add-apt-repository universe
+
 sudo apt-get update
+sudo apt-get upgrade -y
+
+#dep
+sudo apt install git -y
+sudo apt-get install python3-pip -y
+sudo apt-get install python3-venv -y
+
+sudo python3 -m pip install -U pip
+sudo python3 -m pip install -U setuptools
+
+sudo apt-get install libzbar0
 
 # app
 git clone https://github.com/Bekoko/cddesksig.git
 
 # venv
 python3 -m venv env_cddesksig
-source "/home/kali/Desktop/env_cddesksig/bin/activate"
+source "/home/ubuntu/Desktop/env_cddesksig/bin/activate"
 
 # dependencies
-pip install -r "/home/kali/Desktop/cddesksig/requirements.txt"
+pip3 install --upgrade setuptools
+pip3 install --upgrade pip
+pip3 install -r "/home/ubuntu/Desktop/cddesksig/requirements.txt"
 
 # start local server
-python3 cddesksig/manage.py runserver
+python3 "/home/ubuntu/Desktop/cddesksig/manage.py" runserver
 
